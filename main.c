5437151502e98f5308282d68d27e74b89e9d38f1
Agustin <darkstone19@gmail.com>
2023-04-19 21:58:51 -0300

    Primer commit

--- main.c
+++ main.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
+/*
+ EJERCICIO 21 : Se pide replicar el comportamiento de la función de asignación dinámica de memoria (malloc), a través de una función de la siguiente forma: 1. int * overrideMalloc(int, int[]);
 
+Notas:
+
+a. El primer parámetro que se envía es el tamaño del arreglo que se solicita y el segundo es un arreglo que representa la memoria del sistema operativo. Este arreglo contiene unos y ceros, donde un 0 representa un espacio de memoria disponible, y un 1 indica que dicho espacio está siendo utilizado por otro proceso.
+
+b. La función deberá devolver la dirección de memoria a partir de la cual se puede almacenar el bloque de memoria solicitado.
+
+c. En caso de no contar con dicho espacio, la función deberá retornar NULL.
+ */
 int main() {
     printf("Hello, World!\n");
     return 0;
